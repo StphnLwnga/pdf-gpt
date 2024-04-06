e0fdda5cd8aaa2d96c5a80660c684f12f71d0c60
StphnLwnga <boywilder99@gmail.com>
2024-04-06 17:41:13 +0200

    initial UI setup

---
+++ components/toggle-dark-mode.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import * as React from "react"
+import { useState, useEffect } from "react"
+import { useTheme } from "next-themes"
+import { FcIdea, FcNoIdea } from "react-icons/fc"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
+import TooltipComponent from "@/components/tooltip-component"
+
+
+export function ToggleDarkMode(): JSX.Element {
+  const { setTheme, theme } = useTheme();
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = (): void => {
+    setTheme(isDarkTheme ? "light" : "dark");
+    setIsDarkTheme(!isDarkTheme);
+  }
+
+  useEffect(() => {
+    setIsDarkTheme(theme === "dark");
+  }, [theme]);
+
+  return (
+    <div>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="rounded-full"
+        onClick={toggleTheme}
+      >
+        {!isDarkTheme ? (
+          <TooltipComponent
+            tooltipTrigger={
+              <FcNoIdea className="h-[1.2rem] w-[1.2rem] transition-all z-55" />
+            }
+            tooltipContent="Dark Mode"
+          />
+        ) : (
+          <TooltipComponent
+            tooltipTrigger={
+              <FcIdea className="h-[1.2rem] w-[1.2rem] transition-all z-55" />
+            }
+            tooltipContent="Light Mode"
+          />
+        )}
+        <span className="sr-only">Toggle dark mode</span>
+      </Button>
+    </div>
+
+  )
+}
