c83d49ac38ad14019c598fd69f0dc9690549627c
StphnLwnga <boywilder99@gmail.com>
2024-04-09 15:57:37 +0200

    paste link / upload file layout

---
+++ app/_components/pdf-input/PDFInput.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  CalendarIcon,
+  EnvelopeClosedIcon,
+  FaceIcon,
+  GearIcon,
+  PersonIcon,
+  RocketIcon,
+} from "@radix-ui/react-icons"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+
+const PDFInput = () => {
+  return (
+    <div className="width-[80vw] ml-[20vw] pt-[3vw] pr-4 h-[100vh] flex items-center justify-center">
+      <div className="grid w-full max-w-sm items-center justify-center align-middle">
+        <Tabs defaultValue="pasteLink" className="w-[40vw]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="pasteLink">📋 Paste PDF link</TabsTrigger>
+            <TabsTrigger value="uploadPdf">📂 Upload PDF</TabsTrigger>
+          </TabsList>
+          <TabsContent value="pasteLink">
+            <div className="flex w-[40vw] items-center space-x-2">
+              <Input placeholder="Link here 🔗..." className="w-[40vw]" />
+              <Button 
+              type="submit"
+              className="text-xl rounded-full p-1"
+              variant="ghost"
+              >
+                🪄
+              </Button>
+            </div>
+          </TabsContent>
+          <TabsContent value="uploadPdf">
+            <div className="flex  items-center space-x-2 w-[40vw]">
+              <Input
+                id="uploadFile"
+                type="file"
+                className="w-[40vw]"
+              />
+              <Button 
+              type="submit"
+              className="text-2xl rounded-full p-0"
+              variant="ghost"
+              >
+                 🪄
+              </Button>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  )
+}
+
+export default PDFInput;
+
+{/* <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="uploadPDF">Browse</Label>
+        <Input id="uploadPDF" type="file" />
+      </div> */}
