687f6a750ae2813ae88ec23a165e5a61ef3254d7
StphnLwnga <boywilder99@gmail.com>
2024-04-22 21:17:22 +0000

    input layout changes

--- app/_components/pdf-input/PDFInput.tsx
+++ app/_components/pdf-input/PDFInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,15 +7,10 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   CalendarIcon,
   EnvelopeClosedIcon,
@@ -23,7 +18,7 @@ import {
   GearIcon,
   PersonIcon,
   RocketIcon,
-} from "@radix-ui/react-icons"
+} from "@radix-ui/react-icons";
 import {
   Command,
   CommandEmpty,
@@ -33,12 +28,12 @@ import {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 
 const PDFInput = () => {
   return (
-    <div className="width-[80vw] ml-[20vw] pt-[3vw] pr-4 h-[100vh] flex items-center justify-center">
-      <div className="grid w-full max-w-sm items-center justify-center align-middle">
+    <div className="width-[80vw] ml-[20vw] pt-[5vw] pr-4 h-[90vh] flex justify-center">
+      <div className="grid w-full max-w-sm justify-center align-middle">
         <Tabs defaultValue="pasteLink" className="w-[40vw]">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="pasteLink">📋 Paste PDF link</TabsTrigger>
@@ -47,10 +42,10 @@ const PDFInput = () => {
           <TabsContent value="pasteLink">
             <div className="flex w-[40vw] items-center space-x-2">
               <Input placeholder="Link here 🔗..." className="w-[40vw]" />
-              <Button 
-              type="submit"
-              className="text-xl rounded-full p-1"
-              variant="ghost"
+              <Button
+                type="submit"
+                className="text-xl rounded-full p-1"
+                variant="ghost"
               >
                 🪄
               </Button>
@@ -58,29 +53,27 @@ const PDFInput = () => {
           </TabsContent>
           <TabsContent value="uploadPdf">
             <div className="flex  items-center space-x-2 w-[40vw]">
-              <Input
-                id="uploadFile"
-                type="file"
-                className="w-[40vw]"
-              />
-              <Button 
-              type="submit"
-              className="text-2xl rounded-full p-0"
-              variant="ghost"
+              <Input id="uploadFile" type="file" className="w-[40vw]" />
+              <Button
+                type="submit"
+                className="text-2xl rounded-full p-0"
+                variant="ghost"
               >
-                 🪄
+                🪄
               </Button>
             </div>
           </TabsContent>
         </Tabs>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PDFInput;
 
-{/* <div className="grid w-full max-w-sm items-center gap-1.5">
+{
+  /* <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="uploadPDF">Browse</Label>
         <Input id="uploadPDF" type="file" />
-      </div> */}
+      </div> */
+}
