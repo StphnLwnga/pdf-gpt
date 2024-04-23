70a07fa3c037d040881af20e61800540ef0c07fb
StphnLwnga <boywilder99@gmail.com>
2024-04-23 23:51:09 +0000

    sidebar layout changes

--- app/_components/pdf-sidebar/PDFSidebar.tsx
+++ app/_components/pdf-sidebar/PDFSidebar.tsx
@@ -1,28 +1,44 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import React from "react";
+import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { PDFSidebarListItem } from "./PDFSidebarListItem";
 import { LoadingSidebar } from "./Loading";
 
 const PDFSidebar = () => {
+  const router = useRouter();
+
   const { resolvedTheme } = useTheme();
+
+  const [activeItemId, setActiveItemId] = useState<null | number>();
+
+  const handleListItemClick = (pdfTitle: string, pdfId: number) => {
+    setActiveItemId(pdfId);
+    router.push(`/doc/${pdfTitle.toLowerCase().replace(/ /g, "-")}`);
+  };
+
+  useEffect(() => {
+    console.log(window.location);
+    if (window.location.pathname === "/") setActiveItemId(null);
+  }, [activeItemId]);
 
   const pdfFilesList = Array(30)
     .fill("_")
     .map((_, i) => (
       <PDFSidebarListItem
         key={i}
+        isActiveItem={i === activeItemId}
         isDarkTheme={resolvedTheme === "dark"}
         pdfTitle={`PDF Document ${i}`}
         pdfUrl={`https://www.media.wmg-is.com/media/portal/media/cms/docs/201202/curreny-quote-sheet_1330379143004.pdf`}
+        onClick={() => handleListItemClick(`PDF Document ${i}`, i)}
       />
     ));
 
   return (
-    // <div className="flex h-full">
     <div className="h-full min-w-[20vw] flex flex-col pl-4">
       <div className="h-[8vh] mt-[2vh] w-full pr-4 flex items-end justify-center  ">
         <Input
@@ -53,7 +69,6 @@ const PDFSidebar = () => {
         )}
       </div>
     </div>
-    // </div>
   );
 };
 
--- app/_components/pdf-sidebar/PDFSidebarListItem.tsx
+++ app/_components/pdf-sidebar/PDFSidebarListItem.tsx
@@ -1,51 +1,56 @@
 "use client";
 
 import React, { MouseEventHandler } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { FcDocument } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 interface PDFSidebarListItemProps {
-  isDarkTheme: boolean;
   pdfTitle: string;
   pdfUrl: string;
+  isDarkTheme: boolean;
+  isActiveItem: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
 export const PDFSidebarListItem = ({
   isDarkTheme,
+  isActiveItem,
   pdfTitle,
   pdfUrl,
+  onClick,
 }: PDFSidebarListItemProps): JSX.Element => {
   return (
     <Button
       variant="ghost"
-      className="w-full flex items-center justify-start rounded-none pr-2 h-14 pl-0"
+      className={cn(
+        "w-full flex items-center justify-start rounded-none pr-2 h-14 pl-0",
+        isActiveItem && "bg-neutral-100",
+        isActiveItem && isDarkTheme && "bg-neutral-800",
+      )}
+      onClick={onClick}
     >
-      <Link
-        href={`/doc/${pdfTitle.toLowerCase().replace(/ /g, "-")}`}
-        className="w-full flex items-center"
+      <div className="max-w-10">
+        <FcDocument className="mr-2 h-8 w-8 my-2" />
+      </div>
+      <div
+        className={cn(
+          "truncate flex flex-col ",
+          !isDarkTheme && "text-slate-500",
+        )}
       >
-        <div className="max-w-10">
-          <FcDocument className="mr-2 h-8 w-8 my-2" />
-        </div>
-        <div
+        <span className="text-start truncate">{pdfTitle}</span>
+        <span
           className={cn(
-            "truncate flex flex-col ",
-            !isDarkTheme && "text-slate-500",
+            "text-start truncate text-xs italic text-slate-400",
+            !isDarkTheme && "text-slate-400",
           )}
         >
-          <span className="text-start truncate">{pdfTitle}</span>
-          <span
-            className={cn(
-              "text-start truncate text-xs italic text-slate-400",
-              !isDarkTheme && "text-slate-400",
-            )}
-          >
-            {pdfUrl}
-          </span>
-        </div>
-      </Link>
+          {pdfUrl}
+        </span>
+      </div>
     </Button>
   );
 };
