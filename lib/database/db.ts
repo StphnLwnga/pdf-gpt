f4ba229b5638d7bb8e8cd2348cef405c9fe05ba0
StphnLwnga <boywilder99@gmail.com>
2024-05-06 21:02:21 +0000

    Prisma vector store setup

--- lib/database/db.ts
+++ lib/database/db.ts
@@ -1,4 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import {
+  PrismaClient,
+  Prisma,
+  Document_Embeddings,
+  PdfDocument,
+  PdfNote,
+  Question_Answering,
+} from "@prisma/client";
+import { PrismaVectorStore } from "@langchain/community/vectorstores/prisma";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -8,4 +17,18 @@ declare global {
 // globalThis is unaffacted by hot reload
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+export const prismaVectorStore =
+  PrismaVectorStore.withModel<Document_Embeddings>(db).create(
+    new OpenAIEmbeddings(),
+    {
+      prisma: Prisma,
+      tableName: "Document_Embeddings",
+      vectorColumnName: "vector",
+      columns: {
+        id: PrismaVectorStore.IdColumn,
+        content: PrismaVectorStore.ContentColumn,
+      },
+    },
+  );
--- lib/database/prisma-vector-store.ts
+++ lib/database/prisma-vector-store.ts
@@ -1,10 +1,25 @@
 import { Document } from "langchain/document";
-import { PrismaVectorStore, PrismaSqlFilter } from "@langchain/community/vectorstores/prisma";
+import {
+  PrismaVectorStore,
+  PrismaSqlFilter,
+} from "@langchain/community/vectorstores/prisma";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PrismaClient, Prisma, Document_Embeddings, PdfDocument, PdfNote, Question_Answering } from "@prisma/client";
-import { Database } from '@/lib/generated/db-types';
-import { db } from "@/lib/db";
+import {
+  PrismaClient,
+  Prisma,
+  Document_Embeddings,
+  PdfDocument,
+  PdfNote,
+  Question_Answering,
+} from "@prisma/client";
+import { Database } from "@/lib/generated/db-types";
+import { db } from "@/lib/database/db";
 
 export class PrismaSupabaseDatabase {
-	vectorStore: PrismaVectorStore<Database>;
+  vectorStore: PrismaVectorStore<Document_Embeddings, "Document_Embeddings">;
+
+  constructor(vectorStore) {
+    this.vectorStore = vectorStore;
+  }
 }
