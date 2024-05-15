3ec3fb3085415733e48729298a2d737e392315ee
St_Lw <stphnlwnga99@gmail.com>
2024-05-15 18:37:00 +0000

    minor tweaks - store

--- lib/store.ts
+++ lib/store.ts
@@ -1,33 +1,31 @@
-import { create } from 'zustand'
-import { Paper, Note } from './types'
-
+import { create } from "zustand";
+import { Paper } from "./types";
 
 type State = {
-	activePdfId: string | null,
-	pdfDocsArray: Paper[],
-	loadingDoc: boolean,
-}
+  activePdfId: string | null;
+  pdfDocsArray: Paper[];
+  loadingDoc: boolean;
+};
 
 type Action = {
-	setActivePdfId: (pdfId: string | null) => void,
-	setPdfDocsArray: (pdfDocs: Paper[]) => void,
-	updatePdfDocsArray: (pdfDoc: Paper) => void,
-	setLoadingDoc: (loadingDoc: boolean) => void,
-}
+  setActivePdfId: (pdfId: string | null) => void;
+  setPdfDocsArray: (pdfDocs: Paper[]) => void;
+  updatePdfDocsArray: (pdfDoc: Paper) => void;
+  setLoadingDoc: (loadingDoc: boolean) => void;
+};
 
 export const usePdfStore = create<State & Action>((set) => ({
-	activePdfId: null,
-	pdfDocsArray: [],
-	loadingDoc: false,
-	setActivePdfId: (pdfId: string | null) => set((state: State) => ({ activePdfId: pdfId, })),
-	setPdfDocsArray: (pdfDocs: Paper[]) => set((state: State) => ({ pdfDocsArray: pdfDocs, })),
-	updatePdfDocsArray: (pdfDoc: Paper) => {
-		set((state: State) => ({
-			pdfDocsArray: [
-				...state.pdfDocsArray,
-				pdfDoc,
-			],
-		}))
-	},
-	setLoadingDoc: (loadingDoc) => set((state: State) => ({ loadingDoc, })),
+  activePdfId: null,
+  pdfDocsArray: [],
+  loadingDoc: false,
+  setActivePdfId: (pdfId: string | null) =>
+    set((state: State) => ({ activePdfId: pdfId })),
+  setPdfDocsArray: (pdfDocs: Paper[]) =>
+    set((state: State) => ({ pdfDocsArray: pdfDocs })),
+  updatePdfDocsArray: (pdfDoc: Paper) => {
+    set((state: State) => ({
+      pdfDocsArray: [...state.pdfDocsArray, pdfDoc],
+    }));
+  },
+  setLoadingDoc: (loadingDoc) => set((state: State) => ({ loadingDoc })),
 }));
