67824ae177a7e4313ee4268f155e180a4648de0e
St_Lw <stphnlwnga99@gmail.com>
2024-05-25 23:06:43 +0000

    loading components

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Loader2 } from 'lucide-react';
+import { Loader2 } from "lucide-react";
 
-const LoadingPDFViewer = ({ resolvedTheme }: { resolvedTheme?: string }) => {
-	const isDark = resolvedTheme === 'dark';
+const Loading = ({ resolvedTheme }: { resolvedTheme?: string }) => {
+  const isDark = resolvedTheme === "dark";
 
-	return (<div className="p-1 h-full w-full">
-		<Skeleton className="h-full w-full flex flex-col items-center justify-center p-4 gap-y-4" >
-			<div><Loader2 className={cn('text-8xl animate-spin',  )} /></div>
-		</Skeleton>
-	</div>);
+  return (
+    <div className="p-1 h-full w-full">
+      <Skeleton className="h-full w-full flex flex-col items-center justify-center p-4 gap-y-4">
+        <div>
+          <Loader2 className={cn("text-8xl animate-spin")} />
+        </div>
+      </Skeleton>
+    </div>
+  );
 };
 
-export default LoadingPDFViewer;
+export default Loading;
--- components/page-load-backdrop.tsx
+++ components/page-load-backdrop.tsx
@@ -1,21 +1,19 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react';
-import { usePdfStore } from '@/lib/store';
-import { Backdrop } from '@mui/material';
-import { Loader2 } from 'lucide-react';
-
+import React, { useEffect } from "react";
+import { usePdfStore } from "@/lib/store";
+import { Backdrop } from "@mui/material";
+import { Loader2 } from "lucide-react";
 
 const PageLoadBackdrop = ({ pageLoad }: { pageLoad?: boolean }) => {
-	
-	return (
-		<Backdrop
-			sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-			open={pageLoad!}
-		>
-			<Loader2 className="h-24 w-24 stroke-[0.5] animate-spin" />
-		</Backdrop>
-	)
-}
+  return (
+    <Backdrop
+      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={pageLoad! ?? true}
+    >
+      <Loader2 className="h-24 w-24 stroke-[0.5] animate-spin" />
+    </Backdrop>
+  );
+};
 
-export default PageLoadBackdrop
+export default PageLoadBackdrop;
