24299fa1183a0b42c8040bf3c923161fae119e43
St_Lw <stphnlwnga99@gmail.com>
2024-05-26 00:16:58 +0000

    basic AI chat - TBC

--- app/doc/[pdfId]/_components/pdf-chat/pdf-chat.tsx
+++ app/doc/[pdfId]/_components/pdf-chat/pdf-chat.tsx
@@ -1,9 +1,28 @@
+"use client";
+
 import React from "react";
+import { useChat } from "ai/react";
 
 const PDFChat = () => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      PDF Chat
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map((m) => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === "user" ? "User: " : "AI: "}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Query document..."
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 };
