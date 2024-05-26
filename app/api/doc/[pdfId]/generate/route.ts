711b64e55fc40d8230799abeb78ada4d13f54215
St_Lw <stphnlwnga99@gmail.com>
2024-05-26 21:00:24 +0000

    fix - generate AI notes route

--- app/api/doc/[pdfId]/generate/route.ts
+++ app/api/doc/[pdfId]/generate/route.ts
@@ -10,16 +10,29 @@ export async function POST(req: Request): Promise<NextResponse> {
     // const { userId } = auth();
     // if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-    const { id, url, documents } = await req.json();
+    const { id } = await req.json();
 
-    const notes = await generateNotes(id, url, documents);
+    if (!id) return new NextResponse("File not found", { status: 500 });
 
-    await db.PdfNote.create({
-      data: {
-        note: notes,
-        pdfDocumentId: id,
-      },
+    const pdfDoc = await db.pdfDocument.findFirst({
+      where: { id },
+      select: { pdf_text: true },
     });
+
+    if (!pdfDoc) return new NextResponse("File not found", { status: 500 });
+
+    const { pdf_text } = pdfDoc;
+
+    const notes = await generateNotes(pdf_text);
+
+    // console.log({ notes });
+
+    // await db.PdfNote.create({
+    //   data: {
+    //     note: notes,
+    //     pdfDocumentId: id,
+    //   },
+    // });
 
     return NextResponse.json({ notes }, { status: 200 });
   } catch (error) {
